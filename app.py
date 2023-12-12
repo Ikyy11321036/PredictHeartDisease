fea53b29c6d46a5817e7491382a3daa5bff0a255
IKY <if321036@students.del.ac.id>
2023-12-12 15:08:43 +0700

    Update

--- app.py
+++ app.py
@@ -1,111 +1,11 @@
-# from flask import Flask, render_template, request
-# import pandas as pd
-# from sklearn.cluster import KMeans
-
-# app = Flask(__name__)
-
-# # Load the pre-trained KMeans model and data
-# data = pd.read_csv("heart.csv")
-
-# # Select columns for clustering
-# columns_for_clustering = [
-#     "age",
-#     "sex",
-#     "cp",
-#     "trestbps",
-#     "chol",
-#     "fbs",
-#     "restecg",
-#     "thalach",
-#     "exang",
-#     "oldpeak",
-#     "slope",
-#     "ca",
-#     "thal",
-# ]
-# X = data[columns_for_clustering]
-
-# # Initialize and fit the KMeans model
-# kmeans = KMeans(n_clusters=2, random_state=42)
-# kmeans.fit(X)
-# data["cluster"] = kmeans.labels_
-
-
-# @app.route("/")
-# def index():
-#     return render_template("index.html")
-
-
-# @app.route("/predict", methods=["POST"])
-# def predict():
-#     if request.method == "POST":
-#         # Extract user input
-#         age = float(request.form["age"])
-#         sex = float(request.form["sex"])
-#         cp = float(request.form["cp"])
-#         trestbps = float(request.form["trestbps"])
-#         chol = float(request.form["chol"])
-#         fbs = float(request.form["fbs"])
-#         restecg = float(request.form["restecg"])
-#         thalach = float(request.form["thalach"])
-#         exang = float(request.form["exang"])
-#         oldpeak = float(request.form["oldpeak"])
-#         slope = float(request.form["slope"])
-#         ca = float(request.form["ca"])
-#         thal = float(request.form["thal"])
-
-#         # Create a new DataFrame for the user input
-#         user_data = pd.DataFrame(
-#             {
-#                 "age": [age],
-#                 "sex": [sex],
-#                 "cp": [cp],
-#                 "trestbps": [trestbps],
-#                 "chol": [chol],
-#                 "fbs": [fbs],
-#                 "restecg": [restecg],
-#                 "thalach": [thalach],
-#                 "exang": [exang],
-#                 "oldpeak": [oldpeak],
-#                 "slope": [slope],
-#                 "ca": [ca],
-#                 "thal": [thal],
-#             }
-#         )
-
-#         # Make a prediction using the pre-trained KMeans model
-#         prediction = kmeans.predict(user_data[columns_for_clustering])[0]
-
-#         # Determine risk category based on the cluster
-#         risk_category = "Risiko Rendah" if prediction == 0 else "Risiko Tinggi"
-
-#         return render_template(
-#             "result.html",
-#             age=age,
-#             sex=sex,
-#             cp=cp,
-#             trestbps=trestbps,
-#             chol=chol,
-#             fbs=fbs,
-#             restecg=restecg,
-#             thalach=thalach,
-#             exang=exang,
-#             oldpeak=oldpeak,
-#             slope=slope,
-#             ca=ca,
-#             thal=thal,
-#             risk_category=risk_category,
-#         )
-
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
 from flask import Flask, render_template, request
 import pandas as pd
 import numpy as np
 from sklearn.cluster import KMeans
 from sklearn.mixture import GaussianMixture
 from sklearn.cluster import AgglomerativeClustering
+from sklearn.preprocessing import StandardScaler
+
 
 app = Flask(__name__)
 
@@ -130,23 +30,26 @@ def initialize_models():
     # Load the heart dataset
     data = pd.read_csv("heart.csv")
     X = data[features]
-
+    # Normalisasi pada subset fitur yang dipilih
+    scaler = StandardScaler()
+    X_scaled = scaler.fit_transform(X)
     # Inisialisasi pusat klaster menggunakan data ke-166 dan ke-302 untuk K-Means
     acuan_1 = X.iloc[165]
     acuan_2 = X.iloc[302]
     centroids_kmeans = [acuan_1.values, acuan_2.values]
     kmeans = KMeans(n_clusters=2, init=centroids_kmeans, n_init=1)
-    kmeans.fit(X)
+    kmeans.fit(X_scaled)
 
     # Inisialisasi pusat klaster menggunakan data ke-166 dan ke-302 untuk GMM
     acuan_1_gmm = X.iloc[165]
     acuan_2_gmm = X.iloc[302]
     centers_gmm = pd.concat([acuan_1_gmm, acuan_2_gmm], axis=1).T
     gmm = GaussianMixture(n_components=2, means_init=centers_gmm, n_init=1)
-    gmm.fit(X)
+    gmm.fit(X_scaled)
 
     # Hierarchical Clustering dengan metode linkage 'ward'
     hierarchical = AgglomerativeClustering(n_clusters=2, linkage="ward")
+    hierarchical.fit(X_scaled)
 
     return kmeans, gmm, hierarchical
 
@@ -187,16 +90,13 @@ def predict():
 
         # Predict using K-Means
         kmeans_label = get_cluster_label("kmeans", new_data, kmeans, gmm, hierarchical)
-
         # Predict using GMM
         gmm_label = get_cluster_label("gmm", new_data, kmeans, gmm, hierarchical)
 
-        # Predict using Hierarchical Clustering
         hierarchical_label = get_cluster_label(
             "hierarchical", np.array(new_data).reshape(1, -1), kmeans, gmm, hierarchical
         )
 
-        # Map cluster labels to categories
         kmeans_category = map_to_category(kmeans_label[0])
         gmm_category = map_to_category(gmm_label[0])
         hierarchical_category = map_to_category(hierarchical_label[0])
